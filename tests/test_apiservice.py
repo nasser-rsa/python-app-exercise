a33360e24489fe3d0f983b9c1c4226b14d56051d
Nasser Sanchez <nassersanchez@gmail.com>
2023-08-06 13:38:47 +0200

    added test for a 404 and 500 api response

--- tests/test_apiservice.py
+++ tests/test_apiservice.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import csv
 import src.Services.ApiService
+import requests
 
 
 class TestApiService(unittest.TestCase):
@@ -54,6 +55,52 @@ class TestApiService(unittest.TestCase):
                 self.assertEqual(int(data[1]), todo['userId'])
                 self.assertEqual(data[2], todo['title'])
                 self.assertEqual(data[3], str(todo['completed']))
+
+    @patch('src.Services.ApiService.requests.get')
+    def test_run_api_not_found_error(self, mock_get):
+        # Mock the response object with a 404 status code
+        mock_response = MagicMock()
+        mock_response.status_code = 404  # Not Found
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError(
+            "404 Client Error: Not Found")
+        mock_get.return_value = mock_response
+
+        # Call the ApiService.run() method
+        api_service = src.Services.ApiService.ApiService()
+        api_service.run()
+
+        # Assert that the storage folder is created
+        storage_folder = os.path.join(os.getcwd(), 'storage')
+        self.assertTrue(os.path.exists(storage_folder))
+
+        # Assert that no CSV files are created due to the error response
+        for todo in self.todos:
+            file_name = f"{datetime.now().strftime('%Y_%m_%d')}_{todo['id']}.csv"
+            file_path = os.path.join(storage_folder, file_name)
+            self.assertFalse(os.path.exists(file_path))
+
+    @patch('src.Services.ApiService.requests.get')
+    def test_run_internal_server_error(self, mock_get):
+        # Mock the response object with a 500 status code
+        mock_response = MagicMock()
+        mock_response.status_code = 500  # Internal Server Error
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError(
+            "500 Server Error")
+        mock_get.return_value = mock_response
+
+        # Call the ApiService.run() method
+        api_service = src.Services.ApiService.ApiService()
+        api_service.run()
+
+        # Assert that the storage folder is created
+        storage_folder = os.path.join(os.getcwd(), 'storage')
+        self.assertTrue(os.path.exists(storage_folder))
+
+        # Assert that no CSV files are created due to the error response
+        for todo in self.todos:
+            file_name = f"{datetime.now().strftime('%Y_%m_%d')}_{todo['id']}.csv"
+            file_path = os.path.join(storage_folder, file_name)
+            self.assertFalse(os.path.exists(file_path))
 
 
 if __name__ == '__main__':

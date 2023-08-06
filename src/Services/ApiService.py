64f825d06d1108860846c445331e40287378d2f3
Nasser Sanchez <nassersanchez@gmail.com>
2023-08-06 15:48:11 +0200

    Refactor ApiService Class

--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -4,59 +4,57 @@ from datetime import datetime
 import csv
 import requests
 
-URL = 'https://jsonplaceholder.typicode.com/todos/'
-STORAGE_FOLDER = os.path.join(os.getcwd(), 'storage')
-
-
-def fetch_todos():
-    try:
-        response = requests.get(URL)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching from the API: {e}", file=stderr)
-        return None
-
 
 class ApiService:
-    def __init__(self):
-        pass
+    def __init__(self, storage_folder=os.path.join(os.getcwd(), 'storage')):
+        self.url = 'https://jsonplaceholder.typicode.com/todos/'
+        self.storage_folder = storage_folder
+
+    def fetch_todos(self):
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching from the API: {e}")
+            return None
+
+    def create_csv_file(self, todo):
+        try:
+            todo_id = todo['id']
+            user_id = todo['userId']
+            title = todo['title']
+            completed = todo['completed']
+
+            file_name = f"{datetime.now().strftime('%Y_%m_%d')}_{todo_id}.csv"
+            file_path = os.path.join(self.storage_folder, file_name)
+
+            with open(file_path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['id', 'userId', 'title', 'completed'])
+                writer.writerow([todo_id, user_id, title, completed])
+            return True
+        except (KeyError, IOError) as e:
+            print(f"Error processing todo: {e}")
+            return False
 
     def run(self):
         print('Running ApiService')
 
-        if not os.path.exists(STORAGE_FOLDER):
-            os.makedirs(STORAGE_FOLDER)
+        if not os.path.exists(self.storage_folder):
+            os.makedirs(self.storage_folder)
 
-        todos = fetch_todos()
+        todos = self.fetch_todos()
         if todos:
             success = True  # Flag to track if any exception occurred
             for todo in todos:
-                try:
-                    # Check for the required keys and handle errors gracefully
-                    todo_id = todo['id']
-                    user_id = todo['userId']
-                    title = todo['title']
-                    completed = todo['completed']
-
-                    file_name = f"{datetime.now().strftime('%Y_%m_%d')}_{todo_id}.csv"
-                    file_path = os.path.join(STORAGE_FOLDER, file_name)
-
-                    try:
-                        with open(file_path, mode='w', newline='') as file:
-                            writer = csv.writer(file)
-                            writer.writerow(
-                                ['id', 'userId', 'title', 'completed'])
-                            writer.writerow(
-                                [todo_id, user_id, title, completed])
-                    except IOError as e:
-                        print(
-                            f"Error writing to file {file_name}: {e}", file=stderr)
-                        success = False
-
-                except KeyError as e:
-                    print(f"Error processing todo: {e}", file=stderr)
+                if not self.create_csv_file(todo):
                     success = False
 
             if success:
                 print("TODOs fetched and saved as CSV files successfully")
+
+
+if __name__ == "__main__":
+    api_service = ApiService()
+    api_service.run()
